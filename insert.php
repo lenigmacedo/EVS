ce2279454edb99af23d8330809dadc991d4874ad
Lennyk <43722810+lenigmacedo@users.noreply.github.com>
2019-10-18 01:21:42 -0300

    Add files via upload

--- insert.php
+++ insert.php
@@ -5,7 +5,18 @@ include 'conexao.php';
 $codigo = $_POST['codigo'];
 $name = $_POST['name'];
 
-$sql_insert = "INSERT INTO responsibles (codigo,name) values(:codigo,:name);";
+
+$sql_verifica = "SELECT * FROM responsibles WHERE codigo = :codigo";
+
+$stmt = $PDO->prepare($sql_verifica);
+$stmt->bindParam(':codigo',$codigo);
+$stmt->execute();
+
+    if($stmt->rowCount() > 0){
+        $retornoApp = array("ENVIO"=>"CODIGO_EXISTENTE");
+    }else {
+        
+        $sql_insert = "INSERT INTO responsibles (codigo,name) values(:codigo,:name);";
 
 $stmt = $PDO->prepare($sql_insert);
 
@@ -18,7 +29,7 @@ if($stmt->execute()){
 }else {
     $retornoApp = array("ENVIO"=>"ERRO");
 }
-
+    }
 echo json_encode($retornoApp);
 
 ?>
